fb05e1cfb01a69beb1684bbece99ed021f2fcafd
Tracy Lee <tleecoding@gmail.com>
2024-12-08 13:26:47 -0500

    rm health check from container

--- apps/api/src/app/health/health.controller.ts
+++ apps/api/src/app/health/health.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService, HttpHealthIndicator, HealthCheck, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import { HealthCheckService, HealthCheck, TypeOrmHealthIndicator } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
-    private http: HttpHealthIndicator,
+    // private http: HttpHealthIndicator,
     private typeOrmHealthIndicator: TypeOrmHealthIndicator,
   ) {}
 
