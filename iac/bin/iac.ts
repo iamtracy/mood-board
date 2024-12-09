886c2cea80b8742db759cc2ede995808ff791c04
Tracy Lee <tleecoding@gmail.com>
2024-12-09 17:42:53 -0500

    env

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -4,18 +4,8 @@ import { MoodStack } from '../lib/iac-stack'
 
 const app = new cdk.App()
 new MoodStack(app, 'MoodStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
   env: {
-    account: '273354628963',
-    region: 'us-east-1'
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
   },
 })
