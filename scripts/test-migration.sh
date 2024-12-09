d6fbb51c8b5d138fc52ad613f99cd657e0de0afc
Tracy Lee <tleecoding@gmail.com>
2024-12-09 16:53:34 -0500

    setup db ci/ci

--- scripts/test-migration.sh
+++ scripts/test-migration.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Set the environment variables (replace these with actual values or export from your secrets management service)
-DB_HOST=${DB_HOST:-'docker.for.mac.host.internal'}           # Default to 'localhost' if not set
+DB_HOST=${DB_HOST:-'docker.for.mac.host.internal'} 
 DB_PORT=${DB_PORT:-'5432'}                # Default to '5432' if not set
-POSTGRES_USERNAME=${POSTGRES_USERNAME:-'postgres'} # Default to 'postgres' if not set
-POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-'postgres'} # Default to 'your-password' if not set
+POSTGRES_USERNAME=${POSTGRES_USERNAME:-'moodyUser'} # Default to 'postgres' if not set
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-'SuperSecretPassword123'} # Default to 'your-password' if not set
 POSTGRES_DB=${POSTGRES_DB:-'moodboard'} # Default to 'your-database' if not set
+NODE_ENV=${NODE_ENV:-'production'} # Default to 'your-database' if not set
 
 echo "Starting the PostgreSQL container..."
 echo "DB_HOST: $DB_HOST"
@@ -13,8 +13,8 @@ echo "DB_PORT: $DB_PORT"
 echo "POSTGRES_USERNAME: $POSTGRES_USERNAME"
 echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
 echo "POSTGRES_DB: $POSTGRES_DB"
+echo "NODE_ENV: $NODE_ENV"
 
-# Run the Docker container with the environment variables
 docker run -d \
   --name mood-board \
   -e DB_HOST=$DB_HOST \
