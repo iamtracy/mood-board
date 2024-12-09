8b38e35b2c69335b39d65b5d9975a785b5a50876
Tracy Lee <tleecoding@gmail.com>
2024-12-09 12:31:44 -0500

    mg

--- db/typeorm.config.ts
+++ db/typeorm.config.ts
@@ -8,12 +8,20 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 
 const configService = new ConfigService()
 
+const entitiesPath = isDevelopment
+  ? `${__dirname}/../entities/**/*.entity.{ts,js}`
+  : '/app/entities/**/*.entity.{ts,js}'
+
+const migrationsPath = isDevelopment
+  ? `${__dirname}/../db/migrations/*.{ts,js}`
+  : '/app/db/migrations/*.{ts,js}'
+
 export default new DataSource({
   database: configService.get('POSTGRES_DB'),
-  entities: ['/app/entities/**/*.entity.{ts,js}'],
+  entities: [entitiesPath],
   host: configService.get('DB_HOST'),
   logging: isDevelopment,
-  migrations: ['/app/db/migrations/*.ts'],
+  migrations: [migrationsPath],
   password: configService.get('POSTGRES_PASSWORD'),
   port: +(configService.get('DB_PORT') ?? 5432),
   synchronize: isDevelopment,
